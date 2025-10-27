49e7d19ad336c0579d2ba7079235f230bea33251
Aleksander <atrupro202@ieszaidinvergeles.org>
2025-10-27 20:23:18 +0100

    feat: borrate old fields in local

--- ejercicio6.R
+++
@@ -1,8 +0,0 @@
-# ----------------------------------------------------------------
-# Ejercicio 6: Transformación lineal (tipificación)
-# Enunciado:
-#   Transforme el vector x <- 1:6 para que tenga media 0 y desviación
-#   estándar 1 (tipificación).
-
-x <- 1:6
-tipificacion <- (x - mean(x)) / sd(x)
