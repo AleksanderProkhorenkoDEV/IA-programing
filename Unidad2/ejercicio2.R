2e824648aab7f44270e5388d0109f0c37b221b7b
Aleksander <atrupro202@ieszaidinvergeles.org>
2025-11-04 19:57:44 +0100

    feat: start the new exercises

---
+++ Unidad2/ejercicio2.R
@@ -0,0 +1,14 @@
+# ----------------------------------------------------------------
+# Ejercicio 2: Selección de filas por condición
+# Enunciado:
+#   Obtenga las filas de df_alumnos correspondientes
+#   a estudiantes con nota >= 7.
+
+nombre  = c("Ana", "Luis", "Marta", "Jorge", "Eva")
+edad    = c(22, 25, 24, 23, 27)
+nota    = c(8.5, 6.0, 9.2, 4.8, 7.5)
+
+df_alumnos <- data.frame(nombre=nombre, edad=edad, nota=nota)
+
+columnaNotas <- df_alumnos$nota[df_alumnos$nota >= 7]
+
--- Unidad2/primer.R
+++
@@ -1,3 +0,0 @@
-#Encuentra los numeros pares divisibles por tres, menores que mil
-v1 <- array(seq(0,1000, by = 2))
-v1[v1 %% 3 == 0]
