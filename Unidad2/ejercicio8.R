a6325dbaf6508fe804bfa9929c1d0c58e6c075b9
Aleksander <atrupro202@ieszaidinvergeles.org>
2025-11-13 20:48:14 +0100

    fix: error asingando variables

--- Unidad2/ejercicio8.R
+++ Unidad2/ejercicio8.R
@@ -7,12 +7,12 @@
 #     activo = c(TRUE, TRUE, FALSE, TRUE, TRUE)
 #   Elimine todas las filas que tengan NA en la columna temperatura.
 
-sensor_id = c("S1", "S2", "S3", "S4", "S5")
-temperatura = c(20.1, NA, 19.8, NA, 22.5)
-activo = c(TRUE, TRUE, FALSE, TRUE, TRUE)
+sensor_id <- c("S1", "S2", "S3", "S4", "S5")
+temperatura <- c(20.1, NA, 19.8, NA, 22.5)
+activo <- c(TRUE, TRUE, FALSE, TRUE, TRUE)
 
-df_sensores = data.frame(sensor_id=sensor_id, temperatura=temperatura, activo=activo)
-df_sensores = subset(df_sensores, !is.na(temperatura))
+df_sensores <- data.frame(sensor_id=sensor_id, temperatura=temperatura, activo=activo)
+df_sensores <- subset(df_sensores, !is.na(temperatura))
 df_sensores
 
 
