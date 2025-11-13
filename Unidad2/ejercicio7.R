30d211af10b1c973b6d0d2187ab2e087e06e6d32
Aleksander <atrupro202@ieszaidinvergeles.org>
2025-11-13 20:21:01 +0100

    fix: error asingando variables

--- Unidad2/ejercicio7.R
+++ Unidad2/ejercicio7.R
@@ -6,10 +6,10 @@
 #   Añada esta columna a df_alumnos y calcule la nota media por grupo
 #   usando tapply().
 
-nombre  = c("Ana", "Luis", "Marta", "Jorge", "Eva")
-edad    = c(22, 25, 24, 23, 27)
-nota    = c(8.5, 6.0, 9.2, 4.8, 7.5)
-grupo   = c("A", "A", "B", "B", "A")
+nombre  <- c("Ana", "Luis", "Marta", "Jorge", "Eva")
+edad    <- c(22, 25, 24, 23, 27)
+nota    <- c(8.5, 6.0, 9.2, 4.8, 7.5)
+grupo   <- c("A", "A", "B", "B", "A")
 
 df_alumnos <- data.frame(nombre=nombre, edad=edad, nota=nota, grupo=grupo)
 df_alumnos$aprobado <- df_alumnos$nota >= 5
