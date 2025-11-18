0c8885e0127da9217e2282263ae30b2f3bf31d7d
Aleksander <aprokhorenkodev@gmail.com>
2025-11-18 13:36:11 +0100

    chore: re-order the folder and his names

--- Unidad1/ejercicio9.R
+++
@@ -1,7 +0,0 @@
-# ----------------------------------------------------------------
-# Ejercicio 9: Secuencias y repetición + ordenación descendente
-# Enunciado:
-#   Genere un vector que repita la secuencia c("A", "B", "C") 4 veces
-#   y luego ordene el resultado de forma descendente alfabéticamente.
-
-sort(rep(c("A", "B", "C"), times=4))
