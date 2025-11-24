1224a9c8e6b4b9ab81e7fea33c4c95441e2c2e24
Aleksander <aprokhorenkodev@gmail.com>
2025-11-24 11:37:14 +0100

    feat: escalate a vector

---
+++ 1-Unidad/1-volume-exercise/ejercicio5.R
@@ -0,0 +1,8 @@
+# ----------------------------------------------------------------
+# Ejercicio 5: Escalado de valores
+# Enunciado:
+#   Cree un vector v <- c(5, 10, 15, 20, 25) y escálelo de forma
+#   que su suma total sea igual a 1.
+v <- c(5, 10, 15, 20, 25)
+escalar <- v / sum(v)
+sum(escalar)
